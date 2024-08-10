64214e34c6032393ce0b30edb035d47140bcddb1
PerovAndrey <nikita.vasiliev1982@mail.ru>
2024-08-11 01:51:08 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,13 @@
+name = "Andrey"
+print(name)
+name = "Urban"
+print(name)
+date_of_birth = "March 2022"
+dateOfBirth = "March 2023"
+print(date_of_birth)
+print(dateOfBirth)
+#practical task
+Number_of_completed_HA = 12
+Number_Of_Hours_Spent = 1.5
+Course_Name = "Python"
+print("Курс:", Course_Name, ", всего задач:", Number_of_completed_HA, ", затрачено часов:", Number_Of_Hours_Spent, ", среднее время выполнения ", Number_Of_Hours_Spent/Number_of_completed_HA, "часа.")
